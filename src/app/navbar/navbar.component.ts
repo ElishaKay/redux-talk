433df0a1c25a8fbc0bf99a6fa584ae58f0f21ad1
Jesse Pinho <jesse@jessepinho.com>
2016-11-23 20:31:18 +0100

    Add a dropdown to the user profile photo

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $;
 
 @Component({
   selector: 'app-navbar',
@@ -6,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  private name: string;
   private profilePhotoURL: string;
 
   ngOnInit() {
+    this.name = 'Jesse Pinho';
     this.profilePhotoURL = 'https://pbs.twimg.com/profile_images/378800000310650745/5e38031f42fdbacc2c2041f021460f02.jpeg';
   }
 }
