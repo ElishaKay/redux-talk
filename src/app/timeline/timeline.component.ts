20d51ee393afe4b13325e7656f4e05afb5d96afe
Jesse Pinho <jesse@jessepinho.com>
2016-11-23 16:34:36 +0100

    Add random profile photo URLs

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import * as moment from 'moment';
 
+function getProfilePhotoURL(): string {
+  return `https://unsplash.it/48/48/?random=${Math.random()}`;
+}
+
 @Component({
   selector: 'app-timeline',
   templateUrl: './timeline.component.html',
@@ -15,35 +19,35 @@ export class TimelineComponent implements OnInit {
         createdAt: moment().subtract(24, 'minutes').toDate(),
         handle: 'dangerzone',
         name: 'Sterling Archer',
-        profilePhotoURL: '',
+        profilePhotoURL: getProfilePhotoURL(),
         text: 'All I\'ve had today is like six gummy bears and some scotch.',
       },
       {
         createdAt: moment().subtract(13, 'hours').toDate(),
         handle: 'alex',
         name: 'Alexander Hamilton',
-        profilePhotoURL: '',
+        profilePhotoURL: getProfilePhotoURL(),
         text: 'Big duel tonight. Wish me luck! #nervous #sharpshooter',
       },
       {
         createdAt: moment().subtract(19, 'hours').toDate(),
         handle: 'inasunshinestateofmind',
         name: 'Florida Man',
-        profilePhotoURL: '',
+        profilePhotoURL: getProfilePhotoURL(),
         text: 'Catch me on the news tonight at 7pm EST!',
       },
       {
         createdAt: moment().subtract(4, 'days').toDate(),
         handle: 'lowercase',
         name: 'e. e. cummings',
-        profilePhotoURL: '',
+        profilePhotoURL: getProfilePhotoURL(),
         text: 'where is the shift key i cannot find it',
       },
       {
         createdAt: moment().subtract(10, 'days').toDate(),
         handle: '2016',
         name: 'The Year 2016',
-        profilePhotoURL: '',
+        profilePhotoURL: getProfilePhotoURL(),
         text: 'Sorry for everything.',
       },
     ];
