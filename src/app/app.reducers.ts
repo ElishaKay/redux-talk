afe40cde5c4765206b95b32d5da97b571025abed
Jesse Pinho <jesse@jessepinho.com>
2016-12-16 19:25:11 +0100

    Add user reducer; implement ProfileContainerComponent

--- src/app/app.reducers.ts
+++ src/app/app.reducers.ts
@@ -21,7 +21,18 @@ function posts(state = mockPosts, action) {
   return state;
 }
 
+const USER_INITIAL_STATE = {
+  name: 'Jesse Pinho',
+  handle: 'jessepinho',
+  headerPhotoURL: 'https://pbs.twimg.com/profile_banners/16901789/1398787929/1500x500',
+  profilePhotoURL: 'https://pbs.twimg.com/profile_images/378800000310650745/5e38031f42fdbacc2c2041f021460f02.jpeg',
+};
+function user(state = USER_INITIAL_STATE, action) {
+  return state;
+}
+
 export const rootReducer = combineReducers({
   newPostIds,
   posts,
+  user,
 });
--- src/app/profile/profile.container.ts
+++ src/app/profile/profile.container.ts
@@ -1,7 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { NgRedux } from 'ng2-redux';
 
 @Component({
   selector: 'app-profile-container',
-  template: '<app-profile></app-profile>',
+  template: `
+    <app-profile
+      [name]="name"
+      [handle]="handle"
+      [headerPhotoURL]="headerPhotoURL"
+      [profilePhotoURL]="profilePhotoURL"
+      [posts]="posts"
+      [likes]="likes"
+      [reposts]="reposts"
+      ></app-profile>
+  `,
 })
-export class ProfileContainerComponent {}
+export class ProfileContainerComponent implements OnDestroy {
+  private disconnect: Function;
+
+  constructor(
+    private ngRedux: NgRedux<IAppState>
+  ) {
+    this.disconnect = ngRedux.connect(this.mapStateToTarget, null)(this);
+  }
+
+  ngOnDestroy() {
+    this.disconnect();
+  }
+
+  private mapStateToTarget(state: IAppState) {
+    const {
+      posts,
+      user: {
+        name,
+        handle,
+        headerPhotoURL,
+        profilePhotoURL,
+      },
+    } = state;
+
+    return {
+      name,
+      handle,
+      headerPhotoURL,
+      profilePhotoURL,
+
+      posts: posts.filter(post => post.handle === handle).length,
+      likes: posts.filter(post => post.liked).length,
+      reposts: posts.filter(post => post.reposted).length,
+    };
+  }
+}
