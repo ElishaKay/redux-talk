5538df0d7907ff950bbe23b628cc8529a3ee6d42
Jesse Pinho <jesse@jessepinho.com>
2016-12-16 19:57:02 +0100

    Use Redux in the navbar container

--- src/app/navbar/navbar.container.ts
+++ src/app/navbar/navbar.container.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { NgRedux } from 'ng2-redux';
 
 @Component({
   selector: 'app-navbar-container',
-  template: '<app-navbar></app-navbar>'
+  template: `
+    <app-navbar
+      [name]="name"
+      [profilePhotoURL]="profilePhotoURL"
+      ></app-navbar>
+  `,
 })
 export class NavbarContainerComponent {
+  private disconnect: Function;
+
+  constructor(
+    private ngRedux: NgRedux<IAppState>
+  ) {
+    this.disconnect = ngRedux.connect(this.mapStateToTarget, null)(this);
+  }
+
+  ngOnDestroy() {
+    this.disconnect();
+  }
+
+  private mapStateToTarget(state: IAppState) {
+    const { user: { name, profilePhotoURL } } = state;
+
+    return {
+      name,
+      profilePhotoURL,
+    };
+  }
 }
